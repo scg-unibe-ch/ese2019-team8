3fcba2365182ff7eb373179282ed729f3fb5ba03
Lukas Schacher <lukas.schacher@students.unibe.ch>
2019-11-12 13:55:53 +0100

    Added routing-module and basic routing functionalities. Default page is now /login.

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from './Components/login/login.component';
+import {RegistrationComponent} from './Components/registration/registration.component';
+
+
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'registration', component: RegistrationComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,21 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouteReuseStrategy} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 
-import { TodoListComponent } from './Components/todo-list/todo-list.component';
-import { TodoItemComponent } from './Components/todo-list/todo-item/todo-item.component';
+import {TodoListComponent} from './Components/todo-list/todo-list.component';
+import {TodoItemComponent} from './Components/todo-list/todo-item/todo-item.component';
 
 import {ComponentIonChipComponent} from './Components/ion-chip/component-ion-chip.component';
 import {RegistrationComponent} from './Components/registration/registration.component';
 import {LoginComponent} from './Components/login/login.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -31,13 +32,15 @@ import {LoginComponent} from './Components/login/login.component';
     BrowserModule,
     IonicModule.forRoot(),
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    {provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
