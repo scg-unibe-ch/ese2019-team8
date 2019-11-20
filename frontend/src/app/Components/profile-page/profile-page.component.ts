d89a4ff5f8f884213e2607b02ee46606efacd37e
Lukas Schacher <lukas.schacher@students.unibe.ch>
2019-11-20 17:32:13 +0100

    Profile page can now get userdata. Only username implemented yet.

--- frontend/src/app/Components/profile-page/profile-page.component.ts
+++ frontend/src/app/Components/profile-page/profile-page.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 import {UserItem} from '../../_models/user-item';
 import {AuthenticationService} from '../../_services';
 import {HttpClient} from '@angular/common/http';
@@ -17,8 +17,14 @@ export class ProfilePageComponent implements OnInit {
     private httpClient: HttpClient,
     private router: Router,
     private formBuilder: FormBuilder
-  ) {}
+  ) {
+  }
+
+  @Input()
   profilePageForm: FormGroup;
+  user: UserItem;
+  url1 = 'http://localhost:3000/user/profile/';
+  userItem: UserItem = new UserItem(null, '', false, '', '', null, '', null);
 
   ngOnInit() {
     this.profilePageForm = this.formBuilder.group({
@@ -26,6 +32,20 @@ export class ProfilePageComponent implements OnInit {
       email: new FormControl('email@gmail.com'),
       password: new FormControl('pw1234'),
     });
+    // console.log('hoi');
+    this.httpClient.get(this.url1 + localStorage.getItem('currentUser').replace('"', '').replace('"', ''))
+      .subscribe((instance: any) => {
+        // this.user = instances.map((instance) => new userItem(instance.username, instance.email, instance.zip));
+        // this.userItem.username = instance.username;
+        this.userItem.username = instance.username;
+        this.userItem.address = instance.address;
+        this.userItem.email = instance.email;
+        this.userItem.city = instance.city;
+        this.userItem.zip = instance.zip;
+        this.userItem.phoneNumber = instance.phoneNumber;
+        // console.log(instance);
+      });
+    // console.log(this.userItem);
   }
 
 
@@ -33,13 +53,13 @@ export class ProfilePageComponent implements OnInit {
   }
 
 
-
   logout() {
     this.authService.logout();
   }
+
   getUserData() {
-    console.log(this.authService.getUserData());
-    return this.authService.getUserData();
+    // console.log(this.authService.getUserData());
+    // return this.authService.getUserData();
   }
 
 
